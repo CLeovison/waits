1b0ef5f87ab49b88f8aa6b930b35b51fb836b144
Leovison <cleovison@gmail.com>
2024-03-12 16:33:36 +0800

    Added Address

--- src/models/User/Address.js
+++ src/models/User/Address.js
@@ -1,14 +1,17 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const AddressSchema = new mongoose.Schema({
+  street: String,
+  state: String,
+  city: String,
+  region: String,
+});
 
 
-
-})
 
 
 //The Reason Why It Was Written module.exports is because you want to the controller to have a access in the Schema
 //The mongoose.model has been declared to call the MongoDB and put it in the database.
 //After Declaring the function model you need to put the "Address" parameter to let the db know what it is, lastly
-//The Address Schema has been declared in the mongoose model to know what are the data that the db should have 
-module.exports = mongoose.model("Address", AddressSchema)
+//The Address Schema has been declared in the mongoose model to know what are the data that the db should have
+module.exports = mongoose.model("Address", AddressSchema);
