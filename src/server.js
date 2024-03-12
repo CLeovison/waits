8d8dea890d53f980807108860ccdeef9a7576656
Leovison <cleovison@gmail.com>
2024-03-12 18:25:04 +0800

    M

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log("MongoDB connectd"))
 .catch(err => console.log(err))
-
+ 
 app.use('/posts', registerRoutes)
 
 app.listen(PORT, () =>{
