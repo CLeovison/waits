17ba351552360cc99a74251c0c3b27db55101c98
CLeovison <cleovison@gmail.com>
2024-03-13 01:19:01 +0800

    G

--- src/models/User/Address.js
+++ src/models/User/Address.js
@@ -7,9 +7,6 @@ const AddressSchema = new mongoose.Schema({
   region: String,
 });
 
-
-
-
 //The Reason Why It Was Written module.exports is because you want to the controller to have a access in the Schema
 //The mongoose.model has been declared to call the MongoDB and put it in the database.
 //After Declaring the function model you need to put the "Address" parameter to let the db know what it is, lastly
