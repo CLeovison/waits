607eda254a7981cfad359900271bad6aaefe07dd
CLeovison <cleovison@gmail.com>
2024-03-20 00:02:01 +0800

    Debugging

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -4,9 +4,11 @@ const postController = require("../controllers/postController");
 
 router.post("/", postController.createPost);
 router.get("/", postController.getAllPosts);
-router.get("/", postController.getPaginatedPosts);
+router.get("/", postController.getPaginatedPosts); debugger
 router.get("/:id", postController.getPostsByID);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
 
 module.exports = router;
+
+
