4a8877eed9b849bf4584356a8cfa6256962cce52
CLeovison <cleovison@gmail.com>
2024-03-26 00:23:13 +0800

    Update

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express"), router = express.Router();
 const postController = require("../controllers/postController");
 
 router.post("/", postController.createPost);
 router.get("/", postController.getAllPosts);
-router.get("/posts", postController.getPaginatedPosts); 
-router.get("/:id", postController.getPostsByID);
+router.get("/:posts", postController.getPaginatedPosts); 
+// router.get("/:id", postController.getPostsByID);
 router.put("/:id", postController.updatePost);
 router.delete("/:id", postController.deletePost);
 
